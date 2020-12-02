f9a6afcbf662b2b57b231c52bb21186a4acf42c2
Mate <matt.kovr@gmail.com>
2020-12-02 11:33:15 +0100

    2-2

--- Day 2/2-2.py
+++ Day 2/2-2.py
@@ -5,4 +5,18 @@ with open("Day 2/data.txt") as text_file:
     choices = list(text_file.readlines())
     valid_passwords = []
 
-#TBD
+for item in choices:
+    items = item.split(" ")
+    first, last = items[0].split("-")
+    char = items[1].strip(":")
+    string = items[2].strip("\n")
+    first = int(first) - 1
+    last = int(last) - 1
+
+    if string[first] == char and string[last] == char:
+        continue
+
+    if string[first] == char or string[last] == char:
+        valid_passwords.append(string)
+
+print(len(valid_passwords))
