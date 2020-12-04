2593843b351769d7e731106059c25cd3002fad59
Mate <matt.kovr@gmail.com>
2020-12-04 14:01:02 +0100

    3-1

---
+++ Day 4/4-1.py
@@ -0,0 +1,33 @@
+#Advent of Code 2020 Day 4 Part 1
+
+with open('Day 4/data.txt') as file:
+    data = file.readlines()
+    data = [ line.strip() for line in data ]
+
+valid = 0
+
+fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid', 'cid']
+
+def isValid(pp):
+    for field in fields:
+        if field not in pp:
+            return False
+    return True
+
+currentPass = ''
+for line in data:
+    if line != '':
+        currentPass += line
+
+    else:
+        if isValid(currentPass):
+            valid += 1
+
+        currentPass = ''
+
+if isValid(currentPass):
+    valid += 1
+
+print(valid)
+
+# Nem tudom mi a szar nem jó, de esküszöm ráeresztek egy regexet
