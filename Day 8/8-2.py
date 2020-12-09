b13b10ab6484e10544bfd659761ff2df00c55602
Mate <matt.kovr@gmail.com>
2020-12-09 22:21:13 +0100

    8-2

--- Day 8/8-2.py
+++ Day 8/8-2.py
@@ -2,7 +2,7 @@ with open ('Day 8/data.txt') as f:
     d = f.readlines()
     d = [ line.strip() for line in d ]
 
-def get_acc():
+def get_acc_end():
     acc = 0
     line = 0
     inst = []
@@ -22,9 +22,17 @@ def get_acc():
             line += num
         elif cmd == 'nop':
             line += 1
+        if line >= len(d):
+            return acc, True
 
-    return acc
-        
-acc = get_acc()
+    return acc, False
 
-print(acc)
+for i in range(len(d)):
+    if 'jmp' in d[i]:
+        d[i] = d[i].replace('jmp', 'nop')
+        acc, found = get_acc_end()
+        if found:
+            print(acc)
+            break
+        else:
+            d[i] = d[i].replace('nop', 'jmp')
